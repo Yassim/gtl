098cb5a045614942838d2131006a3e3519bf3403
Andrew Phillips <andrew.j.w.phillips@gmail.com>
2015-08-13 08:59:22 +0100

    Moved overlap to cc file

--- include/gtl/utilities.h
+++ include/gtl/utilities.h
@@ -59,17 +59,10 @@ namespace gtl {
         return i_a > i_b ? i_a : i_b;
     }
 
-    static bool overlap(intptr_t i_dst, intptr_t i_src, size_type i_n, size_type i_st)
-    {
-        const size_type s = i_n * i_st;
-        const intptr_t minp = min(i_src, i_dst);
-        const intptr_t maxp = max(i_src + s, i_dst + s);
-        const size_type diff = maxp - minp;
-        return diff < s * 2;
-    }
+    bool overlap(intptr_t i_dst, intptr_t i_src, size_type i_n, size_type i_st);
 
     template<typename T>
-    static bool overlap(T* i_dst, T* i_src, T* i_end)
+    bool overlap(T* i_dst, T* i_src, T* i_end)
     {
         return overlap(reinterpret_cast<intptr_t>(i_dst), reinterpret_cast<intptr_t>(i_src), i_end - i_src, sizeof(T));
     }
---
+++ src/utilities.cc
@@ -0,0 +1,15 @@
+#include "..\include\gtl\utilities.h"
+
+namespace gtl {
+
+bool overlap(intptr_t i_dst, intptr_t i_src, size_type i_n, size_type i_st)
+{
+    const size_type s = i_n * i_st;
+    const intptr_t minp = min(i_src, i_dst);
+    const intptr_t maxp = max(i_src + s, i_dst + s);
+    const size_type diff = maxp - minp;
+    return diff < s * 2;
+}
+
+}
+
