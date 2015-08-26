1b3299c60bc7ebd92c387858df54b14be59720b5
Andrew Phillips <andrew.j.w.phillips@gmail.com>
2015-08-26 09:10:58 +0100

    WIP: have chunk for file based image

--- src/persistant/filebacked_heap.cc
+++ src/persistant/filebacked_heap.cc
@@ -1,4 +1,5 @@
 #include "../../include/gtl/persistant/filebacked_heap.h"
+#include "../../include/gtl/persistant/offset_ptr.h"
 
 namespace gtl {
 namespace persistant {
@@ -39,14 +40,43 @@ image_base::~image_base()
     }
 }
 
-//filebacked_heap::filebacked_heap() {}
-//filebacked_heap::filebacked_heap(const filebacked_heap&) {}
-//filebacked_heap::~filebacked_heap() {}
-//
-//void * filebacked_heap::alloc(size_type i_n)               { return ::malloc(i_n); }
-//void * filebacked_heap::realloc(void * i_p, size_type i_n) { return ::realloc(i_p, i_n); }
-//void filebacked_heap::free(void * i_p)                     { ::free(i_p); }
-//size_type filebacked_heap::max_size(size_type i_n)         { return i_n; }
+struct chunk_type;
+enum {
+    kChunkFlag_Free = 1,
+};
+struct chunk_type {
+    offset_ptr_wflags<chunk_type>   next; // could also be used for size
+    offset_ptr<chunk_type>          prev; 
+
+    inline bool IsFree() const  { return next.flags() & kChunkFlag_Free; }
+    inline void SetFree()       { next.set_flags(kChunkFlag_Free);  }
+    inline size_type Size() const
+    {
+        const chunk_type * n = next;
+        const size_type c = (n - this)-1;
+        return c * sizeof(chunk_type);
+    }
+};
+
+void * image_base::alloc(size_type i_n)
+{
+    return ::malloc(i_n);
+}
+
+void * image_base::realloc(void * i_p, size_type i_n) 
+{
+    void * p = alloc(i_n);
+    memcpy(p, i_p, i_n);
+    free(i_p);
+    return p; 
+}
+
+void image_base::free(void * i_p)
+{
+    ::free(i_p);
+
+}
+size_type image_base::max_size(size_type i_n)         { return i_n; }
 
 }
 }
